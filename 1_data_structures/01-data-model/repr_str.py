a9f2f672a7d896f9d75e2c3935811f10f0b23702
0xdomyz <yzdominic@outlook.com>
2023-09-24 20:53:39 +1000

    upate repr str

--- 1_data_structures/01-data-model/repr_str.py
+++ 1_data_structures/01-data-model/repr_str.py
@@ -1,3 +1,4 @@
+# example1
 class A:
     def __repr__(self):
         return "repr"
@@ -5,14 +6,13 @@ class A:
 
 a = A()
 
-# for developer
-a
-
+a  # for developer
 # for end user
 str(a)  # str() calls __str__ if it exists, otherwise __repr__
 print(a)  # print uses str()
 
 
+# example 2
 class B:
     def __str__(self):
         return "str"
@@ -22,10 +22,47 @@ class B:
 
 
 b = B()
-
-# for developer
 b
-
-# for end user
 str(b)
 print(b)
+
+# example 3
+from collections import namedtuple
+
+a = namedtuple("Point", "x y")(1, "two")
+a
+repr(a)
+str(a)
+print(a)
+
+# namd tuple
+Point = namedtuple("Point", ["x", "y"])
+p = Point(1, 2)
+
+print(repr(p))
+print(str(p))
+
+p.__str__()  # not helpful
+
+# dict
+d = {"a": 1, "b": "two"}
+repr(d)
+str(d)
+
+# dataframe
+import pandas as pd
+
+df = pd.DataFrame({"a": [1, 2], "b": ["two", "three"]})
+repr(df)
+str(df)
+
+large_df = pd.DataFrame({"a": range(1000), "b": range(1000)})
+repr(large_df)
+str(large_df)
+
+# datetime
+import datetime
+
+today = datetime.datetime.now()
+repr(today)
+str(today)
