8bb5c31b47a3bf745134454d4bf4e2fc5af4c756
0xdomyz <yzdominic@outlook.com>
2023-11-23 21:26:27 +1100

    add script

---
+++ 4_control_flow/20-executors/getflags/run.sh
@@ -0,0 +1,15 @@
+
+# sequential
+./flags.py
+
+# concurrent
+./flags_asyncio.py
+./flags_threadpool.py
+./flags_threadpool_futures.py
+
+# errors
+./flags2_asyncio_executor.py
+./flags2_sequential.py
+./flags2_threadpool.py
+./flags3_asyncio.py
+
