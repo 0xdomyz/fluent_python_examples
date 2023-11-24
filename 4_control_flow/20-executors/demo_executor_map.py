007e6b80ee251d2cc68e4a805875bc5dd5102396
0xdomyz <yzdominic@outlook.com>
2023-11-25 01:41:01 +1100

    update demo execu map

--- 4_control_flow/20-executors/demo_executor_map.py
+++ 4_control_flow/20-executors/demo_executor_map.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Experiment with ``ThreadPoolExecutor.map``
 """
@@ -5,27 +7,31 @@ Experiment with ``ThreadPoolExecutor.map``
 from time import sleep, strftime
 from concurrent import futures
 
+
 def display(*args):  # <1>
-    print(strftime('[%H:%M:%S]'), end=' ')
+    print(strftime("[%H:%M:%S]"), end=" ")
     print(*args)
 
+
 def loiter(n):  # <2>
-    msg = '{}loiter({}): doing nothing for {}s...'
-    display(msg.format('\t'*n, n, n))
+    msg = "{}loiter({}): doing nothing for {}s..."
+    display(msg.format("\t" * n, n, n))
     sleep(n)
-    msg = '{}loiter({}): done.'
-    display(msg.format('\t'*n, n))
+    msg = "{}loiter({}): done."
+    display(msg.format("\t" * n, n))
     return n * 10  # <3>
 
+
 def main():
-    display('Script starting.')
+    display("Script starting.")
     executor = futures.ThreadPoolExecutor(max_workers=3)  # <4>
     results = executor.map(loiter, range(5))  # <5>
-    display('results:', results)  # <6>
-    display('Waiting for individual results:')
+    display("results:", results)  # <6>
+    display("Waiting for individual results:")
     for i, result in enumerate(results):  # <7>
-        display(f'result {i}: {result}')
+        display(f"result {i}: {result}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 # end::EXECUTOR_MAP[]
